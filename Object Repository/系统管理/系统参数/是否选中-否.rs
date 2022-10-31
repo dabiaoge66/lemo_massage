50cce3a900036d54c001de605cda2499394af3b4
LMB-QA <LMB-QA@DESKTOP-DQT55Q8>
2022-10-31 13:48:23 +0800

    Revert "Revert "1""
    This reverts commit b2e4e36291ff6fea90d2c76c41cf7f8fbefb7d3c.

---
+++ Object Repository/系统管理/系统参数/是否选中-否.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>是否选中-否</name>
+   <tag></tag>
+   <elementGuidId>b7cedacb-e2fe-4e8e-b0f6-d3f259b1928c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>.layui-btn-group [data-value=&quot;0&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/frames/主页面/系统管理-系统参数</value>
+      <webElementGuid>8813f324-c555-4247-a627-23ceb54b2c05</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
